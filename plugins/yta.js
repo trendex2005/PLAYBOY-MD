fc9da0a8322434953a633530180237fb96560f3f
trendex2005 <Babademmy13@gmail.com>
2025-09-30 23:36:06 +0300

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -32,7 +32,7 @@ cmd({
         await reply("⏳ Downloading audio...");
 
         // Use API to get audio
-        const apiUrl = `https://apis.davidcyriltech.my.id/download/ytmp3?url=${encodeURIComponent(videoUrl)}`;
+        const apiUrl = `https://api.platform.web.id/docs/#/Downloader/get_notube_download${encodeURIComponent(videoUrl)}`;
         const response = await fetch(apiUrl);
         const data = await response.json();
 
