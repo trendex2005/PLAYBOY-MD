4cabf4571972ec99f7514a1121ffa5c8b79e9c07
trendex2005 <Babademmy13@gmail.com>
2025-10-04 22:27:24 +0300

    Update play3.js

--- plugins/play3.js
+++ plugins/play3.js
@@ -32,7 +32,7 @@ cmd({
         await reply("⏳ Downloading audio...");
 
         // Use API to get audio
-        const apiUrl = `https://delirius-apiofc.vercel.app/download/ytmp3?url=${encodeURIComponent(videoUrl)}`;
+        const apiUrl = `https://api.giftedtech.co.ke/api/download/ytmp3?apikey=gifted&url=${encodeURIComponent(videoUrl)}`;
         const response = await fetch(apiUrl);
         const data = await response.json();
 
