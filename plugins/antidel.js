4037de6b8d87d42d1f86802bf96fe677d47951f2
trendex2005 <Babademmy13@gmail.com>
2025-10-20 20:55:29 +0300

    Update antidel.js

--- plugins/antidel.js
+++ plugins/antidel.js
@@ -5,7 +5,7 @@ cmd({
     pattern: "antidelete",
     alias: ['antidel', 'del'],
     desc: "Toggle anti-delete feature",
-    category: "misc",
+    category: "owner",
     filename: __filename
 },
 async (conn, mek, m, { from, reply, text, isCreator }) => {
