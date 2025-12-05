01b01a3813efa3fe6e1c9ca4133f5828521cd532
trendex2030 <bravinnyakundi674@gmail.com>
2025-12-05 22:16:43 +0300

    Update Menu.js

--- plugins/Menu.js
+++ plugins/Menu.js
@@ -329,6 +329,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
+                  image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/adymbp.jpg' },
             
                 caption: dec,
                 contextInfo: {
