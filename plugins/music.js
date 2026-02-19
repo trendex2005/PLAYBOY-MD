966c4c291e08127972581eb1a3d6ce31a7d52b32
trendex2030 <bravinnyakundi674@gmail.com>
2026-02-19 10:26:01 +0300

    Update music.js

--- plugins/music.js
+++ plugins/music.js
@@ -20,7 +20,7 @@ cmd(
     try {
       await reply("🔎 Searching for your song... (this may take a while)");
 
-      const apiUrl = `https://izumiiiiiiii.dpdns.org/downloader/youtube?url=${encodeURIComponent(
+      const apiUrl = `https://api.giftedtech.co.ke/api/search/yts?apikey=gifted&query=${encodeURIComponent(
         text
       )}`;
 
