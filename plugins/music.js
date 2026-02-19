86bf2930ba3d421ecd1664714a962f71d55629ee
trendex2030 <bravinnyakundi674@gmail.com>
2026-02-19 10:32:56 +0300

    Update music.js

--- plugins/music.js
+++ plugins/music.js
@@ -20,7 +20,7 @@ cmd(
     try {
       await reply("🔎 Searching for your song... (this may take a while)");
 
-      const apiUrl = `https://api.giftedtech.co.ke/api/download/ytmp3?apikey=gifted&url=${encodeURIComponent(
+      const apiUrl = `https://api.giftedtech.co.ke/api/download/ytmp3?apikey=gifted&text=${encodeURIComponent(
         text
       )}`;
 
