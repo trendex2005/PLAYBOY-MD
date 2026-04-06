83468d941a28a18619a8431cd42af4573054ebf8
trendex2030 <bravinnyakundi674@gmail.com>
2026-04-06 20:11:06 +0300

    Update yts.js

--- plugins/yts.js
+++ plugins/yts.js
@@ -20,7 +20,7 @@ cmd(
     try {
       await reply("🔎 Searching for your song... (this may take a while)");
 
-      const apiUrl = `https://zdmzfeqrclgdjzhapdxl.supabase.co/functions/v1/api-proxy/api/audio/?query=${encodeURIComponent(
+      const apiUrl = `https://zdmzfeqrclgdjzhapdxl.supabase.co/functions/v1/api-proxy/api/audio?query=${encodeURIComponent(
         text
       )}`;
 
