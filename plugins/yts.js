c723e60a6759fa71e7d0357fbb9d980d0cb5afa4
trendex2030 <bravinnyakundi674@gmail.com>
2026-04-06 16:34:14 +0300

    Update yts.js

--- plugins/yts.js
+++ plugins/yts.js
@@ -20,7 +20,7 @@ cmd(
     try {
       await reply("🔎 Searching for your song... (this may take a while)");
 
-      const apiUrl = `https://api.privatezia.biz.id/api/downloader/ytplaymp3?query=${encodeURIComponent(
+      const apiUrl = `https://apis.davidcyril.name.ng/play?query=${encodeURIComponent(
         text
       )}`;
 
