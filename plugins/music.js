b27d388c42ec721b58522b9ec7098b4078e130d9
trendex2005 <Babademmy13@gmail.com>
2026-04-12 15:55:17 +0300

    Update music.js

--- plugins/music.js
+++ plugins/music.js
@@ -20,7 +20,7 @@ cmd(
     try {
       await reply("🔎 Searching for your song... (this may take a while)");
 
-      const apiUrl = `https://api.vreden.my.id/api/v1/download/play/audio?query=${encodeURIComponent(
+      const apiUrl = `https://api.vreden.my.id/api/v1/download/youtube/audio?query=${encodeURIComponent(
         text
       )}`;
 
