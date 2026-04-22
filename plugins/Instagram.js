3053483585f7757d760bb74ec23cd30811c701d8
trendex2030 <bravinnyakundi674@gmail.com>
2026-04-23 00:47:39 +0300

    Update Instagram.js

--- plugins/Instagram.js
+++ plugins/Instagram.js
@@ -23,7 +23,7 @@ cmd({
       react: { text: "⏳", key: m.key }
     });
 
-    const response = await axios.get(`https://api.davidcyriltech.my.id/instagram?url=${q}`);
+    const response = await axios.get(`https://apiskeith.top/download/instadl?url=${q}`);
     const data = response.data;
 
     if (!data || data.status !== 200 || !data.downloadUrl) {
